2efd8cd9c1f0d1d9d2478e34e718a740bd84520f
Nicolai Lyden <nicolai.lyden@endios.de>
2023-02-27 15:14:21 +0100

    Added code snippets

---
+++ kotlin/ConsumptionOverviewViewModel.kt
@@ -0,0 +1,31 @@
+package de.endios.endiosone.oneWidgetConsumption.css.ui.consumption
+
+internal class ConsumptionOverviewViewModel(private val app: Application) :
+    StatefulViewModel<String>(app) {
+
+    fun updateConsumption(items: List<ConsumptionBodyItem>) {
+        viewModelScope.launch {
+            val result = consentRepository.updateConsent(items)
+            var obisDataNightConsumptionInt = 0
+            var obisDataDayConsumptionInt = 0
+
+            result?.obisDataNight?.previousYear?.consumptionInt.takeIf {
+                it.toString().isNotEmpty()
+            }?.let { value ->
+                obisDataNightConsumptionInt = value
+            }
+
+            result?.obisDataDay?.previousYear?.consumptionInt.takeIf { it.toString().isNotEmpty() }
+                ?.let { value ->
+                    obisDataDayConsumptionInt = value
+                }
+
+            val consumptionText = app.getString(
+                R.string.consumption_css_day_night_consumption_format,
+                obisDataNightConsumptionInt,
+                obisDataDayConsumptionInt
+            )
+            localState.postValue(State.Success(consumptionText))
+        }
+    }
+}
