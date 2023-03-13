199859eedbc15100af9ee376c5a7b0dc604ca4ad
Nicolai Lyden <nicolai.lyden@endios.de>
2023-03-13 14:29:13 +0100

    Added code snippets

--- kotlin/DashboardFragment.kt
+++ kotlin/DashboardFragment.kt
@@ -22,5 +22,69 @@ internal class DashboardFragment : Fragment(R.layout.fragment_epc_dashboard) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            cardviewCssEpcActionPanel.setCardBackgroundColor(Color.parseColor(dashboardConfigsValues?.currentConsumptionLegendColor))
+
+            if (sessionViewModel.selectedMeter?.obisDataList?.get(0)?.priceCapOverview?.isPredictionUnderSubsidizedLimit == false) {
+                fragmentCssEpcTextViewActionPanelTitle.text =
+                    dashboardConfigsValues?.increasedExpenditurePanelHeader
+                fragmentCssEpcTextViewActionPanelSubTitle.text =
+                    dashboardConfigsValues?.increasedExpenditurePanelBody
+            } else {
+                fragmentCssEpcTextViewActionPanelTitle.text =
+                    dashboardConfigsValues?.reducedExpenditurePanelHeader
+                fragmentCssEpcTextViewActionPanelSubTitle.text =
+                    dashboardConfigsValues?.reducedExpenditurePanelBody
+            }
+
+            fragmentCssEpcTariffName.text = sessionViewModel.selectedContract?.type
+            fragmentCssEpcContractNumber.text = sessionViewModel.selectedContract?.label
+            fragmentCssEpcMeterLastReadingDate.text =
+                "${getString(R.string.meter_last_reading_date)} ${sessionViewModel.selectedMeter?.latestReadingDateFormatted}"
+
+            getString(
+                R.string.meter_last_reading_date,
+                sessionViewModel.selectedMeter?.latestReadingDateFormatted
+            )
+
+            barGraphicEpcPrognosePercent.apply {
+                dashboardConfigsValues?.currentConsumptionLegendColor.takeIf {
+                    it.toString().isNotEmpty()
+                }?.let { color ->
+                    firstBarColor = Color.parseColor(color)
+                }
+                dashboardConfigsValues?.projectedConsumptionLegendColor.takeIf {
+                    it.toString().isNotEmpty()
+                }?.let { color ->
+                    secondBarColor = Color.parseColor(color)
+                }
+                dashboardConfigsValues?.subsidizedConsumptionLegendColor.takeIf {
+                    it.toString().isNotEmpty()
+                }?.let { color ->
+                    indicatorTrianglePaintColor = Color.parseColor(color)
+                }
+
+                val subsidizedConsumptionLimit =
+                    sessionViewModel.selectedMeter?.obisDataList?.get(0)?.priceCapOverview?.subsidizedConsumptionLimit
+                val currentConsumption =
+                    sessionViewModel.selectedMeter?.obisDataList?.get(0)?.priceCapOverview?.currentConsumption
+                val predictedConsumption =
+                    sessionViewModel.selectedMeter?.obisDataList?.get(0)?.priceCapOverview?.predictedConsumption
+                val baselineConsumption =
+                    sessionViewModel.selectedMeter?.obisDataList?.get(0)?.priceCapOverview?.baselineConsumption
+
+                firstBarValue = calculateValue(currentConsumption, baselineConsumption)
+                middleBarValue = calculateValue(predictedConsumption, baselineConsumption)
+                indicatorValue = calculateValue(subsidizedConsumptionLimit, baselineConsumption)
+            }
+        }
+    }
+
+    private fun calculateValue(value: Float?, baselineConsumption: Float?): Float {
+        var calculateValue = 0f
+        if (value != null && baselineConsumption != null) {
+            calculateValue = baselineConsumption.takeIf { it != null }?.let { value / it } ?: 0f
+        }
+        return calculateValue
     }
 }
